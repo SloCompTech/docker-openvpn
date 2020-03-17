158a3ad344d3961a719d80e44d1eddde33e0f778
Martin <martin.dagarin@gmail.com>
2020-03-17 15:43:25 +0100

    Added examples

---
+++ root/usr/local/share/docker-openvpn/examples/basic_nat_wlp/README.md
@@ -0,0 +1,20 @@
+# basic_nat_wlp
+
+Features:  
+
+- Works out of the box on bridge or host network
+- NAT (Network translation protocol)
+- Has configuration wizard
+- **WITHOUT** LAN protection (does not allow traffic to LANs connected to server), so you can still access devices in LAN (but **routed** example is recommended, because here traffic is still NAT-ed)
+
+## Configure
+
+``` bash
+ovpn_enconf basic_nat_wlp
+#Protocol udp, tcp, udp6, tcp6 [udp]:
+#VPN network [10.0.0.0]:
+#Port [1194]:
+#Public IP or domain of server: <PUBLIC IP>
+#DNS1 [8.8.8.8]:
+#DNS2 [8.8.4.4]:
+```
---
+++ root/usr/local/share/docker-openvpn/examples/basic_routed/config/openvpn/hooks/up/10-network.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/with-contenv bash
+
+source /app/hookBaseFirewallSetup.sh
+
+#
+#   Network initialization
+#
+echo "Setting up OpenVPN related firewall rules"
+
+# Open OpenVPN port to outside
+ovpn-iptables -A INPUT -p udp -m udp --dport $PORT -j ACCEPT -m comment --comment "Open OpenVPN port"
+
+# Allow Routing Internet <--> VPN network
+ovpn-iptables -A FORWARD -i tun0 -s $NETWORK_ADDRESS/24 -o $OUT_INT -j ACCEPT -m comment --comment "Allow traffic VPN --> Internet"
+ovpn-iptables -A FORWARD -i $OUT_INT -d $NETWORK_ADDRESS/24 -o tun0 -j ACCEPT -m comment --comment "Allow traffic Internet --> VPN"
